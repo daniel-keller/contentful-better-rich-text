3109b1b257340fcf360def5c23510f298a8723d7
Daniel Keller <dkeller@discovernewfields.org>
2024-09-26 00:20:21 -0400

    Refactoring

--- src/components/rich-text/rich-text-types/jest.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-const getBaseConfig = require('../../baseJestConfig');
-const packageJson = require('./package.json');
-const packageName = packageJson.name.split('@contentful/')[1];
-
-module.exports = {
-  ...getBaseConfig(packageName),
-};
